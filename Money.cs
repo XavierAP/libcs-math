ea4f7ae0792c0295b8bedcbed3dc751790833534
XavierAP <n3minis-git@yahoo.es>
2019-01-25 22:03:00 +0100

    Initial library: classes Solve, Money and family of MovingAverages.

---
+++ Money.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace JP.Maths
+{
+	/// <summary>Collection of financial utility methods.</summary>
+	/// <remarks>Rates are per 1, not per cent.</remarks>
+	public static class Money
+	{
+		private const byte
+			precisionDefault = 2, // default number of decimal places for money amounts
+			precisionDefaultPer1 = precisionDefault + 2; // default number of decimal places for rates per 1
+
+		/// <summary>Calculates the discount/interest rate that makes the net present
+		/// value of a set of cash flows equal to a given present value.</summary>
+		/// <param name="flows">Past or future cash flows. Needn't be ordered.
+		/// Must not be empty, and not all elements may have the same Day.
+		/// Cash: positive means money earned, and negative spent.
+		/// Day: only whole days elapsed from this to 'present.Day'
+		/// will be considered in the calculation, not fractions.</param>
+		/// <param name="present">Present Cash value and Day.</param>
+		/// <param name="precision">Precision digits with which the rate is to be found.</param>
+		/// <returns>Yearly rate per 1, rounded off according to 'precision'.</returns>
+		public static double
+		SolveRateInvest(IEnumerable<(double Cash, DateTime Day)> flows,
+			(double Cash, DateTime Day) present, byte precision = precisionDefaultPer1)
+		{
+			// Cache flows into array and translate for more efficient iteration:
+			var flowsDiff = TranslateFlowToDiff(present.Day, flows).ToArray();
+			if(flowsDiff.Length < 1) throw new ArgumentException("Cannot solve discount rate from a single cash flow.", "flows");
+			
+			return Solve.Newton(
+				r => present.Cash + CalcNetPresentValue(r, flowsDiff),
+				r => DerivNetPresentValue(r, flowsDiff),
+				0, precision);
+		}
+
+		/// <summary>Calculates the discount/interest rate that makes zero
+		/// the net present value of a set of cash flows.</summary>
+		/// <param name="flows">Cash flows. Needn't be ordered.
+		/// Must contain more than one element, and not all may have the same Day.
+		/// Cash: positive means money earned, and negative spent.
+		/// Day: fractions of days are not considered.</param>
+		/// <param name="precision">Precision digits with which the rate is to be found.</param>
+		/// <returns>Yearly rate per 1, rounded off according to 'precision'.</returns>
+		public static double
+		SolveRateInvest(IEnumerable<(double Cash, DateTime Day)> flows, byte precision = precisionDefaultPer1)
+		{
+			return SolveRateInvest(flows.Skip(1), flows.First(), precision);  // which time is considered "present" may be arbitrary, it shouldn't affect the solution -- perhaps the convergence.
+		}
+
+
+		/// <summary>Calculates the net present value/worth (NPV) of a set of cash flows.</summary>
+		/// <param name="rate">Interest/discount rate.</param>
+		/// <param name="flowsDiff">Past or future cash flows. Needn't be ordered.
+		/// Cash: positive means money earned, and negative spent.
+		/// Years: elapsed from the time of the flow to the present.</param>
+		public static double
+		CalcNetPresentValue(double rate, IEnumerable<(double Cash, double Years)> flowsDiff)
+		{
+			rate += 1;
+			return (
+				from cf in flowsDiff
+				select cf.Cash * Math.Pow(rate, cf.Years)
+				).Sum();
+		}
+
+		/// <summary>Calculates the net present value/worth of a set of cash flows.</summary>
+		/// <param name="rate">Interest/discount rate.</param>
+		/// <param name="flowsDiff">Past or future cash flows. Needn't be ordered.
+		/// Cash: positive means money earned, and negative spent.
+		/// Day: only whole days elapsed from this to 'presentDate'
+		/// will be considered in the calculation, not fractions.</param>
+		/// <param name="presentDate">Present time.</param>
+		public static double
+		CalcNetPresentValue(double rate, IEnumerable<(double Cash, DateTime Day)> flows, DateTime presentDate)
+		{
+			return CalcNetPresentValue(rate, TranslateFlowToDiff(presentDate, flows));
+		}
+
+		/// <summary>Calculates the derivative of the NPV with respect to the interest rate.</summary>
+		/// <param name="rate">Interest/discount rate; independent variable of the derivative.</param>
+		/// <param name="flowsDiff">Past or future cash flows. Needn't be ordered; parameters.</param>
+		private static double
+		DerivNetPresentValue(double rate, IEnumerable<(double Cash, double Years)> flowsDiff)
+		{
+			rate += 1;
+			return (
+				from cf in flowsDiff
+				select cf.Cash * cf.Years * Math.Pow(rate, cf.Years - 1)
+				).Sum();
+		}
+
+
+		/// <summary>Translates a set of cash flows from (double, DateTime) to (double, Years).
+		/// This improves efficiency in case the NPV of the same cash flows at different rates
+		/// needs to be calculated e.g. by an interative solver.</summary>
+		/// <param name="presentDate">Present time.</param>
+		/// <param name="flows">Cash flows.
+		/// Cash: positive means money earned, and negative spent.
+		/// Day: only whole days elapsed from this to 'presentDate'
+		/// will be considered in the calculation, not fractions.</param>
+		private static IEnumerable<(double Cash, double Years)>
+		TranslateFlowToDiff(DateTime presentDate, IEnumerable<(double Cash, DateTime Day)> flows)
+		{
+			presentDate = presentDate.ToLocalTime(); // DateTimes must be alike referenced before subtracting
+			return (
+				from cf in flows
+				select ( Cash: cf.Cash, Years: (double)(presentDate - cf.Day.ToLocalTime()).Days / 365.25 )
+				);
+		}
+	}
+}
---
+++ Solve.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace JP.Maths
+{
+	using fn = Func<double, double>; // scalar function type
+
+	/// <summary>Solvers for scalar equations.</summary>
+	public static class Solve
+	{
+		/// <summary>Tries to find a root (zero) of f, by Newton's method.</summary>
+		/// <param name="f">Function whose root is to be found.</param>
+		/// <param name="df">Derivative of f.</param>
+		/// <param name="x0">Initial value. Ideally close to the root.</param>
+		/// <param name="precision">Precision digits with which the root is to be found.</param>
+		/// <returns>Root found, rounded off according to 'precision'.</returns>
+		/// <remarks>On failure to find a root, it may return a non-real number, or never
+		/// return (consider running asynchronously and aborting on time out).</remarks>
+		public static double
+		Newton(fn f, fn df, double x0, byte precision)
+		{
+			double tol = Math.Pow(10, -precision) / 4;
+			double x1 = x0;
+			do
+			{
+				x0 = x1;
+				x1 = x0 - f(x0) / df(x0);
+			}
+			while(tol <= Math.Abs(x1 - x0));
+
+			return Math.Round(x1, precision);
+		}
+		
+		/// <summary>Tries to find a root (zero) of f, by the secant method.</summary>
+		/// <param name="f">Function whose root is to be found.</param>
+		/// <param name="x0">Initial value. Ideally close to the root.</param>
+		/// <param name="x1">Another initial value. Ideally close to the root.</param>
+		/// <param name="precision">Precision digits with which the root is to be found.</param>
+		/// <returns>Root found, rounded off according to 'precision'.</returns>
+		/// <remarks>On failure to find a root, it may return a non-real number, or never
+		/// return (consider running asynchronously and aborting on time out).</remarks>
+		public static double
+		Secant(fn f, double x0, double x1, byte precision)
+		{
+			double tol = Math.Pow(10, -precision) / 4;
+			while(tol <= Math.Abs(x1 - x0))
+			{
+				double
+					f0 = f(x0),
+					f1 = f(x1);
+
+				double x2 = x1 - f1 * (x1 - x0) / (f1 - f0);
+
+				x0 = x1;
+				x1 = x2;
+			}
+			return Math.Round(x1, precision);
+		}
+	}
+}
