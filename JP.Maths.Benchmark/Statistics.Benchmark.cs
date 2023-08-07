f6fdcc6137a6ca779dd4c4b4399ea987fabc0354
XavierAP <2506829+XavierAP@users.noreply.github.com>
2023-08-07 21:52:14 +0200

    Surprising results:
            System.Linq:
    Min: -9999999, Max: 89999999, Average: 40000000, TIME: 1,103.00
    Min: -9999999, Max: 89999999, Average: 40000000, TIME: 1,101.00
    Min: -9999999, Max: 89999999, Average: 40000000, TIME: 1,101.00
    Min: -9999999, Max: 89999999, Average: 40000000, TIME: 1,104.00
    Min: -9999999, Max: 89999999, Average: 40000000, TIME: 1,100.00
    Min: -9999999, Max: 89999999, Average: 40000000, TIME: 1,101.00
    Min: -9999999, Max: 89999999, Average: 40000000, TIME: 1,100.00
    Min: -9999999, Max: 89999999, Average: 40000000, TIME: 1,101.00
    Min: -9999999, Max: 89999999, Average: 40000000, TIME: 1,103.00
    Min: -9999999, Max: 89999999, Average: 40000000, TIME: 1,101.00
            JP.Maths.Statistics:
    Min: -9999999, Max: 89999999, Average: 40000000, TIME: 1,571.00
    Min: -9999999, Max: 89999999, Average: 40000000, TIME: 1,467.00
    Min: -9999999, Max: 89999999, Average: 39999999.92663765, TIME: 1,533.00
    Min: -9999999, Max: 89999999, Average: 39999999.81997824, TIME: 1,559.00
    Min: -9999999, Max: 89999999, Average: 39999999.75598259, TIME: 1,562.00
    Min: -9999999, Max: 89999999, Average: 39999999.713318825, TIME: 1,421.00
    Min: -9999999, Max: 89999999, Average: 39999999.682844706, TIME: 1,421.00
    Min: -9999999, Max: 89999999, Average: 39999999.65998912, TIME: 1,459.00
    Min: -9999999, Max: 89999999, Average: 39999999.64221255, TIME: 1,427.00
    Min: -9999999, Max: 89999999, Average: 39999999.62799131, TIME: 1,455.00

---
+++ JP.Maths.Benchmark/Statistics.Benchmark.cs
@@ -0,0 +1,47 @@
+﻿using System.Diagnostics;
+using System.Linq;
+using static System.Console;
+
+namespace JP.Maths.Statistics
+{
+	class Benchmark
+	{
+		static void Main()
+		{
+			var timer = new Stopwatch();
+			const int repeats = 10;
+			var population = Enumerable.Range(-9999999, 99999999).ToArray();
+
+			WriteLine("        System.Linq:");
+			for(int i = 0; i < repeats; i++)
+			{
+				timer.Restart();
+
+				var min = population.Min();
+				var max = population.Max();
+				var avg = population.Average();
+
+				timer.Stop();
+				WriteLine($"Min: {min}, Max: {max}, Average: {avg}, TIME: {timer.ElapsedMilliseconds:N}");
+			}
+			
+			WriteLine("        JP.Maths.Statistics:");
+			var stats = new BatchAggregator();
+			stats.Add<Min>();
+			stats.Add<Max>();
+			stats.Add<Average>();
+
+			for(int i = 0; i < repeats; i++)
+			{
+				timer.Restart();
+
+				foreach (var point in population)
+					stats.Aggregate(point);
+
+				timer.Stop();
+				var (min, max, avg) = (stats.GetResult<Min>(), stats.GetResult<Max>(), stats.GetResult<Average>());
+				WriteLine($"Min: {min}, Max: {max}, Average: {avg}, TIME: {timer.ElapsedMilliseconds:N}");
+			}
+		}
+	}
+}
