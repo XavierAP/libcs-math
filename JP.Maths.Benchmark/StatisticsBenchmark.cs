ec5d1172c96c0b262a9bf250d2e206e6e5e9ff11
XavierAP <2506829+XavierAP@users.noreply.github.com>
2023-08-07 22:45:39 +0200

    Changed Stopwatch for BenchmarkDotNet, same result
    | Method |       Mean |   Error |  StdDev |
    |------- |-----------:|--------:|--------:|
    |   Linq |   222.7 ms | 0.31 ms | 0.74 ms |
    |   Mine | 1,282.3 ms | 1.72 ms | 1.34 ms |
    
    Linq performance does depend on numeric type (int, double).
    And if not cached to array or List,
    Enumerable.Range makes Linq as slow as me.

---
+++ JP.Maths.Benchmark/StatisticsBenchmark.cs
@@ -0,0 +1,53 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using System;
+using System.Linq;
+using static System.Console;
+
+namespace JP.Maths.Statistics
+{
+	using Scalar = Int32;
+
+	public class StatisticsBenchmark
+	{
+		static void Main() => BenchmarkRunner.Run<StatisticsBenchmark>();
+
+		Scalar[] population;
+
+		[GlobalSetup]
+		public void Setup()
+		{
+			population = Enumerable.Range(-9999999, 99999999)
+				.Select(x => (Scalar)x)
+				.ToArray();
+		}
+
+		[Benchmark]
+		public void Linq()
+		{
+			var min = population.Min();
+			var max = population.Max();
+			var avg = population.Average();
+
+			WriteLine($"Min: {min}, Max: {max}, Average: {avg}");
+		}
+		
+		[Benchmark]
+		public void Mine()
+		{
+			var stats = new BatchAggregator();
+			stats.Add<Min>();
+			stats.Add<Max>();
+			stats.Add<Average>();
+
+			foreach (var point in population)
+				stats.Aggregate(point);
+
+			var min = stats.GetResult<Min>();
+			var max = stats.GetResult<Max>();
+			var avg = stats.GetResult<Average>();
+
+			WriteLine($"Min: {min}, Max: {max}, Average: {avg}");
+		}
+	}
+}
