c9e6a3eeb8c9b9710f13b5cb968672f8eaa0e649
XavierAP <2506829+XavierAP@users.noreply.github.com>
2023-08-08 22:09:03 +0200

    Benchmark investigations, part of issue #2
    |             Method |        Mean |     Error |    StdDev |
    |------------------- |------------:|----------:|----------:|
    |          LinqArray |    62.59 ms |  0.277 ms |  0.246 ms |
    |       LinqIterator | 1,281.41 ms | 21.823 ms | 20.413 ms |
    | LinqIteratorCached |   127.64 ms |  0.239 ms |  0.199 ms |
    |          MineArray | 1,298.85 ms |  3.315 ms |  3.101 ms |
    |       MineIterator | 1,736.16 ms | 29.131 ms | 27.249 ms |

--- JP.Maths.Benchmark/StatisticsBenchmark.cs
+++ JP.Maths.Benchmark/StatisticsBenchmark.cs
@@ -1,6 +1,7 @@
 ﻿using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Running;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using static System.Console;
 
@@ -11,20 +12,41 @@ namespace JP.Maths.Statistics
 	public class StatisticsBenchmark
 	{
 		static void Main() => BenchmarkRunner.Run<StatisticsBenchmark>();
-
-		Scalar[] population;
+		
+		IEnumerable<Scalar> populationIterator;
+		Scalar[] populationArray;
 
 		[GlobalSetup]
 		public void Setup()
 		{
-			population = Enumerable.Range(-9999999, 99999999)
-				.Select(x => (Scalar)x)
-				.ToArray();
+			populationIterator = Enumerable.Range(-9999999, 99999999);
+			populationArray = populationIterator.ToArray();
 		}
 
 		[Benchmark]
-		public void Linq()
+		public void LinqArray()
 		{
+			var min = populationArray.Min();
+			var max = populationArray.Max();
+			var avg = populationArray.Average();
+
+			WriteLine($"Min: {min}, Max: {max}, Average: {avg}");
+		}
+
+		[Benchmark]
+		public void LinqIterator()
+		{
+			var min = populationIterator.Min();
+			var max = populationIterator.Max();
+			var avg = populationIterator.Average();
+
+			WriteLine($"Min: {min}, Max: {max}, Average: {avg}");
+		}
+
+		[Benchmark]
+		public void LinqIteratorCached()
+		{
+			var population = populationIterator.ToArray();
 			var min = population.Min();
 			var max = population.Max();
 			var avg = population.Average();
@@ -33,14 +55,32 @@ namespace JP.Maths.Statistics
 		}
 		
 		[Benchmark]
-		public void Mine()
+		public void MineArray()
 		{
 			var stats = new BatchAggregator();
 			stats.Add<Min>();
 			stats.Add<Max>();
 			stats.Add<Average>();
 
-			foreach (var point in population)
+			foreach (var point in populationArray)
+				stats.Aggregate(point);
+
+			var min = stats.GetResult<Min>();
+			var max = stats.GetResult<Max>();
+			var avg = stats.GetResult<Average>();
+
+			WriteLine($"Min: {min}, Max: {max}, Average: {avg}");
+		}
+		
+		[Benchmark]
+		public void MineIterator()
+		{
+			var stats = new BatchAggregator();
+			stats.Add<Min>();
+			stats.Add<Max>();
+			stats.Add<Average>();
+
+			foreach (var point in populationIterator)
 				stats.Aggregate(point);
 
 			var min = stats.GetResult<Min>();
